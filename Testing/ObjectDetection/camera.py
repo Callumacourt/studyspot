5447a8f333e3b8e2d3469a91481a233fae8aaebb
Alejandro Gustafson Palazuelos <gustafsonpalazuelosar@cardiff.ac.uk>
2026-04-29 16:28:13 +0000

    camera class for picamera operation

---
+++ Testing/ObjectDetection/camera.py
@@ -0,0 +1,19 @@
+from picamera import PiCamera
+from time import sleep
+
+class Camera():
+    
+    def __init__(self, res_x=3280, res_y=2464, save_p='', filename='image.jpg'):
+        self.camera = PiCamera(resolution=(res_x,res_y))
+        self.save_path = save_p + filename
+        
+    def test_cap(self):
+        self.camera.start_preview()
+        sleep(5)
+        self.camera.capture(self.save_path)
+        self.camera.stop_preview()
+        return self.save_path
+
+    def std_cap(self):
+        self.camera.capture(self.save_path)
+        return self.save_path

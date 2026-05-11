0dffdd87dc272af445fd9da90d0b47705f0ac6e3
Callum Acourt <callumacourtt@gmail.com>
2026-05-11 10:59:43 +0100

    chore: fix backend prisma client import

--- Website/backend/src/prisma.ts
+++ Website/backend/src/prisma.ts
@@ -1,7 +1,7 @@
 // prisma config file just used for imports
 
 import "dotenv/config";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
